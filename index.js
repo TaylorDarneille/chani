0a0f2bb661e348ac1d49010f1ed9299ea49d4167
Taylor Darneille <taylordarneille@Taylors-MacBook-Pro.local>
2021-07-30 19:24:50 -0700

    stubbed out crud routes

--- index.js
+++ index.js
@@ -2,8 +2,26 @@ const express = require('express')
 const app = express()
 const PORT = 3000
 
-app.get('/test', (req, res, next) => {
-    res.json({'msg': 'testing, testing, 123'})
+app.use(express.urlencoded({ extended: true }))
+
+// get all friends
+app.get('/friend', (req, res, next) => {
+    res.json({'msg': 'retrieving friends list'})
+})
+
+// create a new friend
+app.post('/friend', (req, res, next)=>{
+    res.json({'msg': 'creating a new friend', body: req.body})
+})
+
+// edit an existing friend
+app.put('/friend/:id', (req, res, next)=>{
+    res.json({'msg': `editting friend #${req.params.id}`})
+})
+
+// permanently delete an existing friend
+app.delete('/friend/:id', (req, res, next)=>{
+    res.json({'msg': `deleting friend #${req.params.id}`})
 })
 
 app.listen(PORT, ()=>{console.log('app is up and running')})
