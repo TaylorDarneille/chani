bf3bb0f5670353371d06c0013e2e1ac2d8ddda93
Taylor Darneille <taylordarneille@Taylors-MacBook-Pro.local>
2021-08-02 17:00:05 -0700

    minimal crud functionality complete

--- index.js
+++ index.js
@@ -40,20 +40,6 @@ app.post('/friend', (req, res, next)=>{
 
 // edit an existing friend
 app.put('/friend/:id', (req, res, next)=>{
-    let year = req.body.year ? parseInt(req.body.year) : null
-    let month = req.body.month ? parseInt(req.body.month) : null
-    let day = req.body.day ? parseInt(req.body.day) : null
-    let hour = req.body.hour ? parseInt(req.body.hour) : null
-    let minute = req.body.minute ? parseInt(req.body.minute) : null
-    let updatedFriend = {
-        name: req.body.name,
-        year,
-        month,
-        day,
-        minute,
-        hour,
-        meridiem: req.body.meridiem
-    }
     db.friend.findByPk(req.params.id)
     .then(async friendToUpdate=>{
         friendToUpdate.name = req.body.name
@@ -69,11 +55,27 @@ app.put('/friend/:id', (req, res, next)=>{
             'body': friendToUpdate
         })
     })
+    .catch(err=>{
+        res.json({
+            'msg': 'Error! Friend not created.',
+            'error': err
+        })
+    })
 })
 
 // permanently delete an existing friend
 app.delete('/friend/:id', (req, res, next)=>{
-    res.json({'msg': `deleting friend #${req.params.id}`})
+    db.friend.findByPk(req.params.id)
+    .then(async foundFriend=>{
+        await foundFriend.destroy()
+        res.json({'msg': `deleted friend #${req.params.id}`})
+    })
+    .catch(err=>{
+        res.json({
+            'msg': 'Error! Friend not created.',
+            'error': err
+        })
+    })
 })
 
 app.listen(PORT, ()=>{console.log('app is up and running')})
